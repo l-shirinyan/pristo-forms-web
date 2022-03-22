38d568f4f074f55af0b937291c0dbae1c1dd75c6
l-shirinyan <lusinshirinyan@gmail.com>
2022-03-22 15:40:51 +0400

    fix guideline stepper colors bug

--- src/views/guidelines/GuidelineStepper.vue
+++ src/views/guidelines/GuidelineStepper.vue
@@ -3,7 +3,7 @@
     <ul class="stepper max-w-6xl mx-auto" data-mdb-stepper="stepper">
       <li class="stepper-step opacity-50" :class='$route.path.includes("guideline_third_basic") ? "stepper-active" : "" '>
         <div class="stepper-head mt-3.5">
-          <div class='border-solid border-2 border-midnight rounded-3xl w-44 h-7 flex '>
+          <div class='border-solid border-2 border-midnight dark:border-dark-open-btn rounded-3xl w-44 h-7 flex '>
             <div class="stepper-head-icon dark: bg-midnight dark:bg-dark-open-btn"> 1 </div>
             <div class="stepper-head-text text-sm sm:block  dark:text-dark-open-btn mt-0.5" :class='$route.path.includes("guideline_third_basic") ? "" : "hidden"'> {{ $t("guidelines.basic_data") }} </div>
           </div>
@@ -11,7 +11,7 @@
       </li>
       <li class="stepper-step opacity-50" :class='$route.path.includes("guideline_third_details") ? "stepper-active" : "" '>
         <div class="stepper-head mt-3.5">
-          <div class='border-solid border-2 border-midnight rounded-3xl w-44 h-7 flex'>
+          <div class='border-solid border-2 border-midnight dark:border-dark-open-btn rounded-3xl w-44 h-7 flex'>
             <span class="stepper-head-icon dark: bg-midnight dark:bg-dark-open-btn"> 2 </span>
             <span class="stepper-head-text text-sm sm:block text-midnight dark:text-dark-open-btn mt-0.5" :class='$route.path.includes("guideline_third_details") ? "" : "hidden"'> {{ $t("guidelines.detail_of_findings") }} </span>
           </div>
@@ -19,7 +19,7 @@
       </li>
       <li class="stepper-step opacity-50" :class='$route.path.includes("guideline_third_safety") ? "stepper-active" : "" '>
         <div class="stepper-head mt-3.5">
-          <div class='border-solid border-2 border-midnight rounded-3xl w-44 h-7 flex'>
+          <div class='border-solid border-2 border-midnight dark:border-dark-open-btn rounded-3xl w-44 h-7 flex'>
             <span class="stepper-head-icon dark: bg-midnight dark:bg-dark-open-btn"> 3 </span>
             <span class="stepper-head-text text-sm sm:block text-midnight dark:text-dark-open-btn mt-0.5" :class='$route.path.includes("guideline_third_safety") ? "" : "hidden"'> {{ $t("guidelines.safety_certificates") }} </span>
           </div>
@@ -27,7 +27,7 @@
       </li>
       <li class="stepper-step opacity-50" :class='$route.path.includes("guideline_third_control") ? "stepper-active" : "" '>
         <div class="stepper-head mt-3.5">
-          <div class='border-solid border-2 border-midnight rounded-3xl w-44 h-7 flex'>
+          <div class='border-solid border-2 border-midnight dark:border-dark-open-btn rounded-3xl w-44 h-7 flex'>
             <span class="stepper-head-icon dark: bg-midnight dark:bg-dark-open-btn"> 4 </span>
             <span class="stepper-head-text text-sm sm:block text-midnight dark:text-dark-open-btn mt-0.5" :class='$route.path.includes("guideline_third_control") ? "" : "hidden"'> {{ $t("guidelines.control_findings") }} </span>
           </div>
@@ -55,17 +55,26 @@
   .stepper-active .stepper-head-icon {
     background-color: #190E66;
   }
+  .dark   .stepper-active .stepper-head-icon {
+    background-color: #7B95FF;
+  }
   .stepper:not(.stepper-vertical) .stepper-step:not(:last-child) .stepper-head:after {
     height: 2px;
     width: 68px;
     background-color: #190E66;
     margin-left: 0;
   }
+  .dark .stepper:not(.stepper-vertical) .stepper-step:not(:last-child) .stepper-head:after {
+    background-color: #7B95FF;
+  }
    .stepper:not(.stepper-vertical) .stepper-step:not(:first-child) .stepper-head:before {
      height: 2px;
      width: 68px;
      background-color: #190E66;
      margin-right: 0;
+   }
+   .dark .stepper:not(.stepper-vertical) .stepper-step:not(:first-child) .stepper-head:before {
+     background-color: #7B95FF;
    }
   .stepper:not(.stepper-vertical) .stepper-step:first-child .stepper-head {
     padding-left: 5px;
